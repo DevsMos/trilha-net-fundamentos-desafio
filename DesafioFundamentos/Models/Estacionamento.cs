2c2f8eb388c0d4329059f024ed633de026f3ab1d
DevsMos <moisesinfoestudante@gmail.com>
2023-12-31 04:28:16 -0300

    Desafio Concluido git add DesafioFundamentos/Models/Estacionamento.cs!

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -11,12 +11,22 @@ namespace DesafioFundamentos.Models
             this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
         }
-
+  
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            String placa = Console.ReadLine();
+            if (veiculos.Any(v => v.Equals(placa.ToUpper())))
+        {
+            Console.WriteLine("já existe um veiculo com essa placa");
+        }
+        else
+        {
+            veiculos.Add(placa);
+            Console.WriteLine("veiculo estacionado com sucesso!");
+        }
         }
 
         public void RemoverVeiculo()
@@ -25,7 +35,7 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -35,8 +45,10 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal valorTotal = precoInicial +precoPorHora*horas;
+
+            veiculos.Remove(placa.ToUpper()); 
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
